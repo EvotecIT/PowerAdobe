456ce906229f101993ac80cb0637570b8bf45e03
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-23 14:35:18 +0200

    feat: Add Set-AdobeUser function to update Adobe user information

---
+++ Public/Set-AdobeUser.ps1
@@ -0,0 +1,78 @@
+﻿function Set-AdobeUser {
+    <#
+    .SYNOPSIS
+    Update Adobe User information using Adobe API
+
+    .DESCRIPTION
+    Update Adobe User information using Adobe API. Applies only to Enterprise and Federated users
+    Independent Adobe IDs are managed by the individual user and cannot be updated through the User Management API.
+    Attempting to update information for a user who has an Adobe ID will result in an error.
+
+    .PARAMETER EmailAddress
+    Parameter description
+
+    .PARAMETER NewEmailAddress
+    Parameter description
+
+    .PARAMETER Country
+    Parameter description
+
+    .PARAMETER FirstName
+    Parameter description
+
+    .PARAMETER LastName
+    Parameter description
+
+    .EXAMPLE
+    $SetInformaiton = Set-AdobeUser -EmailAddress 'przemek@test.pl' -NewEmailAddress 'przemek@test1.pl' -LastName 'Klys' -WhatIf -Verbose
+    $SetInformaiton
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory)][string] $EmailAddress,
+        [string] $NewEmailAddress,
+        [string] $Country,
+        [string] $FirstName,
+        [string] $LastName,
+        [string] $UserName
+    )
+
+    if (-not $Script:AdobeTokenInformation) {
+        Write-Warning -Message 'Set-AdobeUser - You need to connect to Adobe first using Connect-Adobe'
+        return
+    }
+
+    $UpdateObject = [ordered] @{
+        "update" = [ordered] @{
+            email     = $NewEmailAddress
+            country   = $Country
+            firstname = $FirstName
+            lastname  = $LastName
+            username  = $UserName
+        }
+    }
+
+    Remove-EmptyValue -Hashtable $UpdateObject -Recursive -Rerun 2
+    if (-not $UpdateObject) {
+        Write-Warning -Message 'Set-AdobeUser - You need to provide at least one value to update'
+        return
+    }
+    $Data = [ordered] @{
+        user      = $EmailAddress
+        requestID = "1234567890"
+        do        = @(
+            $UpdateObject
+        )
+    }
+
+    $Data | ConvertTo-Json -Depth 5 | Write-Verbose
+
+    $QueryParameter = [ordered] @{
+        testOnly = if ($PSCmdlet.ShouldProcess($EmailAddress, 'Update Adobe User')) { $false } else { $true }
+    }
+
+    Invoke-AdobeQuery -Url "action" -Method 'POST' -Data $Data -QueryParameter $QueryParameter
+}

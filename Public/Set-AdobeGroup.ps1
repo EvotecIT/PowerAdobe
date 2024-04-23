b81155466e0e56f67c0c55ef9af1990c6e5bee4c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-23 21:53:24 +0200

    feat: Add Set-AdobeGroup function to update Adobe user group
    The Set-AdobeGroup function is added to update an existing Adobe user group. It takes parameters for the group's name, new name, and description. The function checks if the user is connected to Adobe using the Connect-Adobe command. It then constructs the necessary data and sends a request to update the group in the organization. The testOnly query parameter is used to determine if the function should be executed or just tested.

---
+++ Public/Set-AdobeGroup.ps1
@@ -0,0 +1,39 @@
+﻿function Set-AdobeGroup {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory)][string] $Name,
+        [Parameter()][string] $NewName,
+        [Parameter()][string] $Description
+    )
+    if (-not $Script:AdobeTokenInformation) {
+        Write-Warning -Message 'Set-AdobeGroup - You need to connect to Adobe first using Connect-Adobe'
+        return
+    }
+    if (-not $NewName -and -not $Description) {
+        Write-Warning -Message 'Set-AdobeGroup - You need to provide either a new name or a description'
+        return
+    }
+
+    $Data = [ordered] @{
+        usergroup = $Name
+        requestID = "action_1"
+        do        = @(
+            [ordered] @{
+                'updateUserGroup' = [ordered] @{
+                    name        = $NewName
+                    description = $Description
+                }
+            }
+        )
+    }
+
+    Remove-EmptyValue -Hashtable $Data -Recursive -Rerun 2
+
+    $Data | ConvertTo-Json -Depth 5 | Write-Verbose
+
+    $QueryParameter = [ordered] @{
+        testOnly = if ($PSCmdlet.ShouldProcess($Name, 'Set Adobe Group')) { $false } else { $true }
+    }
+
+    Invoke-AdobeQuery -Url "action" -Method 'POST' -Data $Data -QueryParameter $QueryParameter
+}

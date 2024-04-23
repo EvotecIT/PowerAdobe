d402d3ba9fba5f7ed7863cc0a09b9b337a7760c4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-23 14:34:54 +0200

    feat: Add Remove-AdobeUser command to remove Adobe user
    The Remove-AdobeUser command is added to remove a user from Adobe. It takes a mandatory parameter for the email address of the user and an optional parameter to specify whether to delete the user account. The command checks if the user is connected to Adobe using the Connect-Adobe command. It then constructs the necessary data and sends a request to remove the user from the organization. The testOnly query parameter is used to determine if the command should be executed or just tested.

---
+++ Public/Remove-AdobeUser.ps1
@@ -0,0 +1,32 @@
+﻿function Remove-AdobeUser {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory)][string] $EmailAddress,
+        [switch] $DoNotDeleteAccount
+    )
+    if (-not $Script:AdobeTokenInformation) {
+        Write-Warning -Message 'Remove-AdobeUser - You need to connect to Adobe first using Connect-Adobe'
+        return
+    }
+    $Data = [ordered] @{
+        user      = $EmailAddress
+        requestID = "action_1"
+        do        = @(
+            [ordered] @{
+                'removeFromOrg' = [ordered] @{
+                    deleteAccount = -not $DoNotDeleteAccount.IsPresent
+                }
+            }
+        )
+    }
+
+    Remove-EmptyValue -Hashtable $Data -Recursive -Rerun 2
+
+    $Data | ConvertTo-Json -Depth 5 | Write-Verbose
+
+    $QueryParameter = [ordered] @{
+        testOnly = if ($PSCmdlet.ShouldProcess($EmailAddress, 'Remove Adobe User')) { $false } else { $true }
+    }
+
+    Invoke-AdobeQuery -Url "action" -Method 'POST' -Data $Data -QueryParameter $QueryParameter
+}

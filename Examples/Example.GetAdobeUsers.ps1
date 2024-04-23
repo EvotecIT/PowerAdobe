e301b7d535df666e70e9bcd3c93e39a9a22f020e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-23 09:39:58 +0200

    Update example

---
+++ Examples/Example.GetAdobeUsers.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module .\PowerAdobe.psd1 -Force
+
+$connectAdobeSplat = @{
+    ClientID     = '464'
+    ClientSecret = 'p8e'
+    Scopes       = 'openid, AdobeID, user_management_sdk'
+    Organization = '783'
+}
+
+$Authorization = Connect-Adobe @connectAdobeSplat -Verbose
+$Authorization | Format-Table
+
+$Users = Get-AdobeUser
+$Users | Format-Table

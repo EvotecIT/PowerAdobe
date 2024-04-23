88484db517cc5a33db6985dc7da2fe058b5382b1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-23 16:50:32 +0200

    feat: Add New-AdobeGroup function to create a new Adobe user group

---
+++ Public/New-AdobeGroup.ps1
@@ -0,0 +1,44 @@
+﻿function New-AdobeGroup {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory)][string] $Name,
+        [Parameter()][string] $Description,
+        [ValidateSet('ignoreIfAlreadyExists', 'updateIfAlreadyExists')]
+        [string] $Option = 'ignoreIfAlreadyExists'
+    )
+
+    $OptionList = @{
+        ignoreIfAlreadyExists = 'ignoreIfAlreadyExists'
+        updateIfAlreadyExists = 'updateIfAlreadyExists'
+    }
+    $OptionConverted = $OptionList[$Option]
+
+    if (-not $Script:AdobeTokenInformation) {
+        Write-Warning -Message 'Get-AdobeUser - You need to connect to Adobe first using Connect-Adobe'
+        return
+    }
+
+    $Data = [ordered] @{
+        usergroup = $Name
+        requestID = "action_1"
+        do        = @(
+            [ordered] @{
+                'createUserGroup' = [ordered] @{
+                    name        = $Name
+                    description = $Description
+                    option      = $OptionConverted
+                }
+            }
+        )
+    }
+
+    Remove-EmptyValue -Hashtable $Data -Recursive -Rerun 2
+
+    #$Data | ConvertTo-Json -Depth 5 | Write-Verbose
+
+    $QueryParameter = [ordered] @{
+        testOnly = if ($PSCmdlet.ShouldProcess($Name, 'Create Adobe Group')) { $false } else { $true }
+    }
+
+    Invoke-AdobeQuery -Url "action" -Method 'POST' -Data $Data -QueryParameter $QueryParameter
+}

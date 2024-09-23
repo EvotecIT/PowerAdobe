900a5d7612241dc49899570243d2fbee9ed68f0e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-23 17:12:10 +0200

    Group Member

--- Public/Get-AdobeGroupMember.ps1
+++ Public/Get-AdobeGroupMember.ps1
@@ -1,6 +1,12 @@
 ﻿function Get-AdobeGroupMember {
     [CmdletBinding()]
     param(
-
+        [parameter(Mandatory)][string] $GroupName
     )
+    if (-not $Script:AdobeTokenInformation) {
+        Write-Warning -Message 'Get-AdobeGroup - You need to connect to Adobe first using Connect-Adobe'
+        return
+    }
+
+    Invoke-AdobeQuery -Url "users/{orgId}/{page}/$GroupName" -Method 'GET'
 }

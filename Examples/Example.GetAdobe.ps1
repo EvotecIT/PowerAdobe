1edbb2eff2285f9348067cd636524276fd8a4065
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-23 17:09:37 +0200

    Fix example

---
+++ Examples/Example.GetAdobe.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module .\PowerAdobe.psd1 -Force
+
+$connectAdobeSplat = @{
+    ClientID     = '464'
+    ClientSecret = 'p8e'
+    Scopes       = 'openid, AdobeID, user_management_sdk'
+    Organization = '783'
+}
+
+Connect-Adobe @connectAdobeSplat -Verbose
+
+# Keep in mind of thresholds which are very strict for all users
+$Users = Get-AdobeUser
+$Users | Format-Table
+
+$User = Get-AdobeUser -Email '<email>' -Verbose
+$User | Format-Table
+
+# Keep in mind of thresholds which are very strict for all groups
+$Groups = Get-AdobeGroup -Verbose
+$Groups | Format-Table
+
+$GroupMembers = Get-AdobeGroupMember -GroupName 'Admins' -Verbose
+$GroupMembers | Format-Table

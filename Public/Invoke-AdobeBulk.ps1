5bed39b3ede92fcc6a7c823a1e27418d34cbb089
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-27 19:08:24 +0100

    Add Invoke-AdobeBulk function for bulk processing of actions

---
+++ Public/Invoke-AdobeBulk.ps1
@@ -0,0 +1,33 @@
+﻿function Invoke-AdobeBulk {
+    [CmdletBinding(supportsShouldProcess)]
+    param(
+        [scriptblock] $Actions,
+        [switch] $Suppress
+    )
+    if (-not $Script:AdobeTokenInformation) {
+        Write-Warning -Message 'Invoke-AdobeBulk - You need to connect to Adobe first using Connect-Adobe'
+        return
+    }
+
+    $AllActions = & $Actions
+    [Array] $ActionsToExecute = foreach ($Action in $AllActions) {
+        $Action
+    }
+
+    $ActionsToExecute | ConvertTo-Json -Depth 5 | Write-Verbose
+
+    $QueryParameter = [ordered] @{
+        testOnly = if ($PSCmdlet.ShouldProcess("Updates", 'Do bulk updates')) { $false } else { $true }
+    }
+
+    $Output = Invoke-AdobeQuery -Url "action" -Method 'POST' -Data $ActionsToExecute -QueryParameter $QueryParameter
+    if ($Output) {
+        foreach ($ErrorMessage in $Output.Errors) {
+            Write-Warning -Message "Invoke-AdobeBulk - Processing error [user: $($ErrorMessage.User), index: $($ErrorMessage.Index)] - $($ErrorMessage.Message)"
+        }
+        if ($Suppress) {
+            return
+        }
+        $Output
+    }
+}
